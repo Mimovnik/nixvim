9fad7f10f1e1b1b46827b3d3b962bebd95756462
mimovnik <mimovnik@protonmail.com>
2024-05-24 18:44:06 +0200

    Configure completion, change cmp bindings

--- config/plugins.nix
+++ config/plugins.nix
@@ -126,18 +126,25 @@
       autoEnableSources = true;
       settings = {
         sources = [
-          {name = "nvim_lsp";}
           {name = "path";}
           {name = "buffer";}
+          {name = "luasnip";}
+          {name = "nvim_lsp";}
+          {name = "nvim_lsp_signature_help";}
         ];
+
+        snippet = {
+          expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+        };
+
         mapping = {
           "<C-Space>" = "cmp.mapping.complete()";
           "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-e>" = "cmp.mapping.close()";
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
           "<CR>" = "cmp.mapping.confirm({ select = true })";
-          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          "<Tab>" = "cmp.mapping.confirm({ select = true })";
+          "<C-p>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<C-n>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
         };
       };
     };
