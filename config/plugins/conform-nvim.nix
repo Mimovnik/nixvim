bfad3f2bffe1058db1924369255fc679cface917
mimovnik <mimovnik@protonmail.com>
2024-07-25 14:56:58 +0200

    feat: remove format on save

--- config/plugins/conform-nvim.nix
+++ config/plugins/conform-nvim.nix
@@ -1,10 +1,6 @@
 {
   plugins.conform-nvim = {
     enable = true;
-    formatOnSave = {
-      lspFallback = true;
-      timeoutMs = 500;
-    };
     notifyOnError = true;
     formattersByFt = {
       nix = ["alejandra"];
