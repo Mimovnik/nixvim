fa82b1c78e966fee9fe4b414c2fa4a93676c0f8a
mimovnik <mimovnik@protonmail.com>
2025-01-03 19:01:12 +0100

    feat: Remove angular ls

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -7,18 +7,4 @@
       clangd.enable = true;
     };
   };
-
-  # IMPURE
-  # angular-language-server can be installed via npm install -g @angular/language-server
-  extraConfigLua = ''
-    -- configure angular-language-server
-    local project_library_path = vim.fn.getcwd()
-    local cmd = {"ngserver", "--stdio", "--tsProbeLocations", project_library_path , "--ngProbeLocations", project_library_path}
-    require'lspconfig'.angularls.setup{
-      cmd = cmd,
-      on_new_config = function(new_config,new_root_dir)
-        new_config.cmd = cmd
-      end,
-    }
-  '';
 }
