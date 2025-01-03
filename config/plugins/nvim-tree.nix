e584528e7ac166d78cfc2afd4b9bed3d99e87900
mimovnik <mimovnik@protonmail.com>
2025-01-03 19:01:48 +0100

    feat(nvim-tree): uncollapse current file

--- config/plugins/nvim-tree.nix
+++ config/plugins/nvim-tree.nix
@@ -2,5 +2,11 @@
   plugins.nvim-tree = {
     enable = true;
     autoClose = true;
+    hijackCursor = true;
+    updateFocusedFile.enable = true;
+    git = {
+      enable = true;
+      ignore = true;
+    };
   };
 }
